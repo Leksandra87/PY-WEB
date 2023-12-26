ef20d931d9aa757db129875b7ac1c8903f8ab2f2
Leksandra87 <petroselinuma@gmail.com>
2023-12-26 20:47:29 +0300

    init

--- login/views.py
+++ login/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'login/index.html')
 
 # Create your views here.
