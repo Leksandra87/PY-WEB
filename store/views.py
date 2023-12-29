c5da14c623ac4de0b31f5a77938f1c096013091b
Leksandra87 <petroselinuma@gmail.com>
2023-12-29 18:24:46 +0300

    init

---
+++ store/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class CartView(View):
+    def get(self, request):
+        return render(request, 'store/cart.html')
+
+
+class ProductView(View):
+    def get(self, request):
+        return render(request, 'store/product-single.html')
+
+
+class ShopView(View):
+    def get(self, request):
+        return render(request, 'store/shop.html')
+
+# Create your views here.
